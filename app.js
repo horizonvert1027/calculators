88baca2df98e183c954eb834c8cd7c6c4bb25fe0
horizonvert1027 <horizonvert.pu@gmail.com>
2023-11-27 10:40:03 +0200

    update port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const express = require('express');
 const path = require('path');
 const cors =  require('cors');
 const app = express();
-const port = 3000;
+const port = 80;
 
 const calculators = [
     'gpa-calculator',
